4874cef066aecf8a37adc56be7c65fec20a81992
sulisak <sarlemonvannasing@gmail.com>
2023-12-08 09:46:17 +0700

    update ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,56 @@
-import React, {useState, useEffect} from 'react'
-
+// import './styles.css';
+import React, { useState, useEffect } from "react";
+import "./assets/styles.css";
 export default function App() {
-  const [users, setUsers] = useState([])
-  useEffect(() => 
-  {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
     console.log(import.meta.env.VITE_API);
-    
-    fetch(import.meta.env.VITE_API+'/buy_details',
-      )
-      .then(res => res.json())
-      .then(result => {
-        setUsers(result)
-        console.log(result)
-      })
-  }, [])
+
+    fetch(import.meta.env.VITE_API + "/buy_details")
+      .then((res) => res.json())
+      .then((result) => {
+        setUsers(result);
+        console.log(result);
+      });
+  }, []);
   return (
-<table>
-  
     <div>
-      <h1>Buy details</h1>
-      <thead>
-      <ul>
-      {users.map(buy => (
-        <li><span>Id:</span> {buy.id}
-         
-        </li>
- 
-      ))}
-      {users.map(user => (
-        <li> <span>Buyer name:</span>{user.username}
-         
-        </li>
-      ))}
-      {users.map(product => (
-        <li> <span>Buy Product name:</span>{product.product}
-         
-        </li>
- 
-      ))}
-      
-      {users.map(buy => (
-            <li> <span>User buy email:</span>{buy.useremail}
-            </li>
-      ))}
-    </ul>
-  </thead>
+      <h1 className="h1">Buy details</h1>
+      <table
+        className="bordered-table"
+        style={{
+          borderCollapse: "solid",
+          width: "100%",
+          border: "2px solid blue",
+          textAlign: "center",
+        }}
+      >
+        <thead
+          style={{
+            borderCollapse: "solid",
+            width: "100%",
+            border: "1px solid blue",
+            textAlign: "center",
+          }}
+        >
+          <tr>
+            <th>Id</th>
+            <th>Buyer name</th>
+            <th>User buy email</th>
+            <th>Buy Product name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((buy) => (
+            <tr key={buy.id}>
+              <td>{buy.id}</td>
+              <td>{buy.username}</td>
+              <td>{buy.useremail}</td>
+              <td>{buy.product}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-    </table>
-  )
+  );
 }
